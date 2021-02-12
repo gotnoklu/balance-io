8ab63b8aacd53851525ad63f4ca6e595ec9d1c9b
Eli <cyronix787@gmail.com>
2021-02-12 11:38:56 +0000

    add global utilities for main process

--- main/utilities/global/index.js
+++ main/utilities/global/index.js
@@ -1,8 +1,8 @@
-const crypto = require('crypto')
-const fs = require('fs')
-const zlib = require('zlib')
+const crypto = require( 'crypto' )
+const fs = require( 'fs' )
+const zlib = require( 'zlib' )
 
-export const createCharacterString = (length) => {
+export const createCharacterString = length => {
 	const characters = [
 		'a',
 		'b',
@@ -42,119 +42,119 @@ export const createCharacterString = (length) => {
 		'9',
 	]
 	let result = []
-	while (result.length < length) {
-		result.push(characters[Math.floor(characters.length * Math.random())])
+	while ( result.length < length ) {
+		result.push( characters[Math.floor( characters.length * Math.random() )] )
 	}
-	return result.join('')
+	return result.join( '' )
 }
 
-export const createId = (length) => createCharacterString(length)
+export const createId = length => createCharacterString( length )
 
-export const createBase64Id = (length) => {
-	const pseudoRandomKey = createId(length)
-	const hash = crypto.createHash('sha256').update(pseudoRandomKey).digest('base64')
+export const createBase64Id = length => {
+	const pseudoRandomKey = createId( length )
+	const hash = crypto.createHash( 'sha256' ).update( pseudoRandomKey ).digest( 'base64' )
 	return hash
 }
 
-export const createObjectClone = (...sources) => Object.assign({}, ...sources)
+export const createObjectClone = ( ...sources ) => Object.assign( {}, ...sources )
 
-export const findIndexFromArray = (list, predicate) => list.findIndex(predicate)
+export const findIndexFromArray = ( list, predicate ) => list.findIndex( predicate )
 
-export const getValueOfKey = (object, keyPath) => {
-	const keys = splitObjectKeys(keyPath)
+export const getValueOfKey = ( object, keyPath ) => {
+	const keys = splitObjectKeys( keyPath )
 	let counter = 0
 	let current = object
-	while (counter < keys.length - 1) {
-		current = Object.getOwnPropertyDescriptor(current, keys[counter]).value
+	while ( counter < keys.length - 1 ) {
+		current = Object.getOwnPropertyDescriptor( current, keys[counter] ).value
 		counter += 1
 	}
 	return current
 }
 
-export const splitObjectKeys = (keyString, delimiter = '.') => keyString.split(delimiter)
+export const splitObjectKeys = ( keyString, delimiter = '.' ) => keyString.split( delimiter )
 
-export const setValueOfKey = (object, keyPath, value) => {
-	const objectClone = createObjectClone(object)
-	const keys = splitObjectKeys(keyPath)
+export const setValueOfKey = ( object, keyPath, value ) => {
+	const objectClone = createObjectClone( object )
+	const keys = splitObjectKeys( keyPath )
 	let counter = 0
 	let current = objectClone
-	while (counter < keys.length - 1) {
-		current = Object.getOwnPropertyDescriptor(current, keys[counter]).value
+	while ( counter < keys.length - 1 ) {
+		current = Object.getOwnPropertyDescriptor( current, keys[counter] ).value
 		counter += 1
 	}
 	current[keys[keys.length - 1]] = value
 	return objectClone
 }
 
-export const createResponse = (data, success = false, error = false) => ({ success, data, error })
+export const createResponse = ( data, success = false, error = false ) => ( { success, data, error } )
 
-export const createErrorResponse = (message) => createResponse(message, false, true)
+export const createErrorResponse = message => createResponse( message, false, true )
 
-export const createNotificationBody = (title, subtitle, message, icon, urgency, timeoutType) => ({
+export const createNotificationBody = ( title, subtitle, message, icon, urgency, timeoutType ) => ( {
 	title,
 	subtitle,
 	body: message,
 	icon,
 	timeoutType,
 	urgency,
-})
+} )
 
-export const writeToFile = (path, data) => {
+export const writeToFile = ( path, data ) => {
 	try {
-		fs.writeFileSync(path, JSON.stringify(data))
-	} catch (error) {
-		fs.appendFileSync(path, JSON.stringify(data))
+		fs.writeFileSync( path, JSON.stringify( data ) )
+	} catch ( error ) {
+		fs.appendFileSync( path, JSON.stringify( data ) )
 	}
 }
 
-export const createDirectory = (dirPath) => {
-	if (!fs.existsSync(dirPath)) {
+export const createDirectory = dirPath => {
+	if ( !fs.existsSync( dirPath ) ) {
 		try {
-			fs.mkdirSync(dirPath)
-			return createResponse('Directory created.', true)
-		} catch (error) {
-			return fs.mkdir(dirPath, (err) => {
-				if (err) return false
-			})
+			fs.mkdirSync( dirPath )
+			return createResponse( 'Directory created.', true )
+		} catch ( error ) {
+			return fs.mkdir( dirPath, err => {
+				if ( err ) return false
+			} )
 		}
 	}
-	return createResponse('Directories exist.', true)
+	return createResponse( 'Directories exist.', true )
 }
 
-export const parseFile = (filePath, defaults) => {
+export const parseFile = ( filePath, defaults ) => {
 	try {
-		return JSON.parse(fs.readFileSync(filePath))
-	} catch (error) {
+		return JSON.parse( fs.readFileSync( filePath ) )
+	} catch ( error ) {
 		return defaults
 	}
 }
 
-export const createFileBackup = (filePath, backupPath) => {
+export const createFileBackup = ( filePath, backupPath ) => {
 	try {
-		if (fs.existsSync(filePath)) {
+		if ( fs.existsSync( filePath ) ) {
 			const gzip = zlib.createGzip()
-			const storeData = fs.createReadStream(filePath)
-			const compressedData = fs.createWriteStream(backupPath, { autoClose: true })
-			storeData.pipe(gzip).pipe(compressedData)
+			const storeData = fs.createReadStream( filePath )
+			const compressedData = fs.createWriteStream( backupPath, { autoClose: true } )
+			storeData.pipe( gzip ).pipe( compressedData )
 			return true
 		}
 		return false
-	} catch (error) {
+	} catch ( error ) {
 		return false
 	}
 }
 
-export const readFileBackup = (filePath, backupPath, fallbackData) => {
+export const readFileBackup = ( filePath, backupPath, fallbackData ) => {
 	try {
-		if (fs.existsSync(backupPath)) {
+		if ( fs.existsSync( backupPath ) ) {
 			const unzip = zlib.createUnzip()
-			const compressedData = fs.createReadStream(backupPath)
-			const storeData = fs.createWriteStream(filePath)
-			compressedData.pipe(unzip).pipe(storeData)
-			return parseFile(filePath, fallbackData)
+			const compressedData = fs.createReadStream( backupPath )
+			const storeData = fs.createWriteStream( filePath )
+			compressedData.pipe( unzip ).pipe( storeData )
+			return parseFile( filePath, fallbackData )
 		}
 		return false
-	} catch (error) {
+	} catch ( error ) {
 		return fallbackData
 	}
 }
