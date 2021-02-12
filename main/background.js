b5d91bb9a2fbe5c1b9419a304f158ad5e38b735e
Eli <cyronix787@gmail.com>
2021-02-12 11:36:33 +0000

    rewrite code

--- main/background.js
+++ main/background.js
@@ -1,60 +1,59 @@
 import { app } from 'electron'
 import serve from 'electron-serve'
-import { createWindow, createTray, createTrayContextMenu } from './utilities/window'
+import { createWindow, createTray, createTrayContextMenu } from './utilities/app'
 import path from 'path'
 import AppStore from './utilities/storage/app-store'
 import handleRendererProcessEvents from './event-handlers'
 
 const isProduction = process.env.NODE_ENV === 'production'
 
-if (isProduction) {
-	serve({ directory: 'app' })
+if ( isProduction ) {
+	serve( { directory: 'app' } )
 } else {
-	app.setPath('userData', `${app.getPath('userData')} (development)`)
+	app.setPath( 'userData', `${app.getPath( 'userData' )} (development)` )
 }
 
-const mainAppStore = new AppStore({
+const mainAppStore = new AppStore( {
 	storeName: 'MainAppStore',
 	defaults: {
 		app: {
 			theme: 'LIGHT',
 			backupType: 'MANUAL',
+			autoBackupDelay: null,
 		},
 		tasks: [],
 	},
-})
+} )
 
 let mainWindow, tray
 
 const initializeApp = async () => {
 	await app.whenReady()
 
-	mainWindow = createWindow('main', {
+	mainWindow = createWindow( 'main', {
 		width: 900,
 		height: 700,
 		minWidth: 700,
 		title: 'Iris',
-		icon: path.join(process.resourcesPath, 'favicon.ico'),
+		icon: path.join( process.resourcesPath, 'favicon.ico' ),
 		webPreferences: {
 			nodeIntegration: true,
 			devTools: true,
 			// preload: path.join(__dirname, 'preload.js'),
 		},
-	})
+	} )
 
-	mainWindow.on('close', (event) => {
-		if (!app.isQuiting) {
+	mainWindow.on( 'close', event => {
+		if ( !app.isQuiting ) {
 			event.preventDefault()
 			mainWindow.hide()
 			return false
 		}
-	})
+	} )
 
-	tray = createTray('tray.png')
-
-	tray.setToolTip('Iris')
-
-	createTrayContextMenu(tray, [
+	tray = createTray( 'tray.png' )
+	tray.setToolTip( 'Iris' )
+	createTrayContextMenu( tray, [
 		{
 			label: 'Open Iris',
 			click() {
@@ -68,30 +67,32 @@ const initializeApp = async () => {
 				app.quit()
 			},
 		},
-	])
+	] )
 
-	tray.on('click', () => {
+	tray.on( 'click', () => {
 		mainWindow.show()
-	})
+	} )
 
-	if (isProduction) {
-		mainWindow.setMenu(null)
-		await mainWindow.loadURL('app://./index.html')
+	if ( isProduction ) {
+		mainWindow.setMenu( null )
+		await mainWindow.loadURL( 'app://./index.html' )
 	} else {
 		const port = process.argv[2]
-		await mainWindow.loadURL(`http://localhost:${port}/`)
+		await mainWindow.loadURL( `http://localhost:${port}/` )
 		mainWindow.webContents.openDevTools()
 	}
 
-	app.setAppUserModelId('app.iris.goals')
+	// Set appUserModelId
+	app.setAppUserModelId( 'app.iris.goals' )
 }
 
 // Initialize app
 initializeApp()
 
 // Handle all events from renderer process
-handleRendererProcessEvents(mainAppStore)
+handleRendererProcessEvents( mainAppStore )
 
+// Quit app after all windows are closed
 // app.on('window-all-closed', () => {
 // 	app.quit()
 // })
