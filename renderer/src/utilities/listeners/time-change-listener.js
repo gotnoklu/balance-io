35f4c51a07c3e841ed742e5a72d38c4e89cd003f
Eli <cyronix787@gmail.com>
2021-02-12 11:48:52 +0000

    create class method

--- renderer/src/utilities/listeners/time-change-listener.js
+++ renderer/src/utilities/listeners/time-change-listener.js
@@ -3,29 +3,49 @@ class TimeChangeListener {
 		this.id = null
 	}
 
-	createListener(triggerDate, callback, delay) {
+	createOneTimeListener( triggerDate, callback, delay ) {
 		try {
-			if (typeof window !== 'undefined') {
+			if ( typeof window !== 'undefined' ) {
 				const getCurrentDate = () => new Date()
-				const convertedDate = triggerDate instanceof Date ? triggerDate : new Date(triggerDate)
+				const convertedDate = triggerDate instanceof Date ? triggerDate : new Date( triggerDate )
 
 				const checkTime = () => {
 					const currentDate = getCurrentDate()
-					if (currentDate >= convertedDate) {
-						if (callback) callback(this.id)
+					if ( currentDate >= convertedDate ) {
+						if ( callback ) callback( this.id )
 						this.removeListener()
 					}
 				}
 
-				this.id = window.setInterval(checkTime, delay)
+				this.id = window.setInterval( checkTime, delay )
 			}
-		} catch (error) {
-			console.log(error)
+		} catch ( error ) {
+			console.log( error )
+		}
+	}
+
+	createRepeatingListener( triggerDate, callback, delay ) {
+		try {
+			if ( typeof window !== 'undefined' ) {
+				const getCurrentDate = () => new Date()
+				const convertedDate = triggerDate instanceof Date ? triggerDate : new Date( triggerDate )
+
+				const checkTime = () => {
+					const currentDate = getCurrentDate()
+					if ( currentDate >= convertedDate ) {
+						if ( callback ) callback( this.id )
+					}
+				}
+
+				this.id = window.setInterval( checkTime, delay )
+			}
+		} catch ( error ) {
+			console.log( error )
 		}
 	}
 
 	removeListener() {
-		window.clearInterval(this.id)
+		if ( this.id !== null ) window.clearInterval( this.id )
 	}
 }
 
