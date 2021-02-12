01e52be7032006895ac5e69d6a3857a8836d1d75
Eli <cyronix787@gmail.com>
2021-02-12 14:16:39 +0000

    read from backup when store is inaccessible

--- main/event-handlers.js
+++ main/event-handlers.js
@@ -12,33 +12,50 @@ import {
 import { createResponse, createErrorResponse, createNotificationBody } from './utilities/global'
 import path from 'path'
 
-const handleRendererProcessEvents = (store) => {
+const handleRendererProcessEvents = store => {
 	// Renderer action handlers
-	ipcMain.handle(GET_MAIN_APP_STORE, () => store.getAll())
+	ipcMain.handle( GET_MAIN_APP_STORE, () => {
+		const storeResponse = store.getAll()
 
-	ipcMain.handle(DELETE_FROM_MAIN_APP_STORE, (event, { key }) => store.delete(key))
+		if ( storeResponse.success ) return storeResponse
 
-	ipcMain.handle(CLEAR_MAIN_APP_STORE, () => store.clear())
+		const readFromBackupResponse = store.readFromBackup()
+		if ( readFromBackupResponse.success ) {
+			store.setStore( readFromBackupResponse.data )
+			return readFromBackupResponse
+		} else {
+			try {
+				store.setStore( {} )
+				return store.getAll()
+			} catch ( error ) {
+				console.log( error )
+			}
+		}
+	} )
 
-	ipcMain.handle(SAVE_TO_MAIN_APP_STORE, (event, { key, value }) => {
-		const setResponse = store.set(key, value)
-		if (setResponse.success) {
+	ipcMain.handle( DELETE_FROM_MAIN_APP_STORE, ( event, { key } ) => store.delete( key ) )
+
+	ipcMain.handle( CLEAR_MAIN_APP_STORE, () => store.clear() )
+
+	ipcMain.handle( SAVE_TO_MAIN_APP_STORE, ( event, { key, value } ) => {
+		const setResponse = store.set( key, value )
+		if ( setResponse.success ) {
 			return store.getAll()
 		}
 		return setResponse
-	})
+	} )
 
-	ipcMain.handle(GET_FROM_MAIN_APP_STORE, (event, key) => store.get(key))
+	ipcMain.handle( GET_FROM_MAIN_APP_STORE, ( event, key ) => store.get( key ) )
 
-	ipcMain.handle(SET_BACKUP_MAIN_APP_STORE, () => store.backup())
+	ipcMain.handle( SET_BACKUP_MAIN_APP_STORE, () => store.backup() )
 
-	ipcMain.handle(GET_BACKUP_MAIN_APP_STORE, () => store.readFromBackup())
+	ipcMain.handle( GET_BACKUP_MAIN_APP_STORE, () => store.readFromBackup() )
 
-	ipcMain.handle(SHOW_APP_NOTIFICATION, (event, { title, description, message }) => {
+	ipcMain.handle( SHOW_APP_NOTIFICATION, ( event, { title, description, message } ) => {
 		try {
-			if (Notification.isSupported()) {
+			if ( Notification.isSupported() ) {
 				const icon = nativeImage.createFromPath(
-					path.join(process.resourcesPath, 'notification.png')
+					path.join( process.resourcesPath, 'notification.png' )
 				)
 				const notificationBody = createNotificationBody(
 					title,
@@ -48,14 +65,14 @@ const handleRendererProcessEvents = (store) => {
 					'critical',
 					'never'
 				)
-				const notification = new Notification(notificationBody)
+				const notification = new Notification( notificationBody )
 				notification.show()
-				return createResponse('Notification success!', true)
+				return createResponse( 'Notification success!', true )
 			}
-		} catch (error) {
-			return createErrorResponse('Could not show app notification.')
+		} catch ( error ) {
+			return createErrorResponse( 'Could not show app notification.' )
 		}
-	})
+	} )
 }
 
 export default handleRendererProcessEvents
