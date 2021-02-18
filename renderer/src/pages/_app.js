4c5965230dfb19c561ccbc2174d41f25ef497bbc
Eli <cyronix787@gmail.com>
2021-02-18 19:25:22 +0000

    configure page byp age config

--- renderer/src/pages/_app.js
+++ renderer/src/pages/_app.js
@@ -1,20 +1,85 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import { makeStyles } from '@material-ui/core/styles'
+import Toolbar from '@material-ui/core/Toolbar'
+import BoardSettingsIcon from '@material-ui/icons/Dashboard'
+import SettingsIcon from '@material-ui/icons/Settings'
+import clsx from 'clsx'
 import Head from 'next/head'
-import CustomCssBaseline from '../components/CustomCssBaseline'
+import { useRouter } from 'next/router'
+import PropTypes from 'prop-types'
+import React from 'react'
+import AppBar from '../components/ui/AppBar'
+import BoardDrawer from '../components/ui/BoardDrawer'
+import CustomCssBaseline from '../components/ui/CustomCssBaseline'
+import { pageConfigByRoute } from '../config/pages'
+import { drawerWidth, settingTypes } from '../constants'
 import AppThemeProvider, { AppThemeConsumer } from '../themes'
-import storeWrapper from '../utilities/store/redux/store'
+import storeWrapper from '../storage/redux/store'
+import { useSelector } from 'react-redux'
+import { getAppSettingOptions, getCurrentBoard } from '../storage/redux/selectors'
 
-function MyApp(props) {
+const settingOptions = {
+	[settingTypes.SETTINGS]: {
+		id: settingTypes.SETTINGS,
+		icon: <SettingsIcon color='secondary' fontSize='small' />,
+		label: 'Settings',
+	},
+	[settingTypes.APP]: {
+		id: settingTypes.APP,
+		icon: <SettingsIcon color='secondary' fontSize='small' />,
+		label: 'Iris Settings',
+	},
+	[settingTypes.BOARD]: {
+		id: settingTypes.BOARD,
+		icon: <BoardSettingsIcon color='secondary' fontSize='small' />,
+		label: 'Board Settings',
+	},
+}
+
+function MyApp( props ) {
+	const [isDrawerOpen, setIsDrawerOpen] = React.useState( false )
+
 	const { Component, pageProps } = props
 
-	React.useEffect(() => {
-		// Remove the server-side injected CSS.
-		const jssStyles = document.querySelector('#jss-server-side')
-		if (jssStyles) {
-			jssStyles.parentElement.removeChild(jssStyles)
+	const router = useRouter()
+	const classes = useStyles( {
+		hasDrawer: pageConfigByRoute[router.pathname].drawer,
+	} )
+	const currentBoard = useSelector( getCurrentBoard )
+	const settings = useSelector( getAppSettingOptions )
+
+	const handleToggleDrawer = () => setIsDrawerOpen( !isDrawerOpen )
+
+	const renderAppBar = () => {
+		const { pathname } = router
+		if ( pageConfigByRoute[pathname].header ) {
+			return (
+				<AppBar
+					drawerOpen={isDrawerOpen}
+					onDrawerToggle={handleToggleDrawer}
+					settings={settings.map( option => {
+						if ( option !== settingTypes.BOARD ) return settingOptions[option]
+						if ( currentBoard && option === settingTypes.BOARD ) return settingOptions[option]
+						return null
+					} )}
+				/>
+			)
 		}
-	}, [])
+	}
+
+	const renderDrawer = () => {
+		const { pathname } = router
+		if ( pageConfigByRoute[pathname].drawer ) {
+			return <BoardDrawer open={isDrawerOpen} />
+		}
+	}
+
+	React.useEffect( () => {
+		// Remove the server-side injected CSS.
+		const jssStyles = document.querySelector( '#jss-server-side' )
+		if ( jssStyles ) {
+			jssStyles.parentElement.removeChild( jssStyles )
+		}
+	}, [] )
 
 	return (
 		<React.Fragment>
@@ -24,8 +89,20 @@ function MyApp(props) {
 			<AppThemeProvider>
 				<CustomCssBaseline />
 				<AppThemeConsumer>
-					{({ setAppTheme, toggleAppTheme }) => (
-						<Component setAppTheme={setAppTheme} toggleTheme={toggleAppTheme} {...pageProps} />
+					{( { setTheme } ) => (
+						<div className={classes.root}>
+							<main
+								className={clsx( classes.content, {
+									[classes.contentShift]: isDrawerOpen,
+								} )}>
+								<React.Fragment>
+									{renderDrawer()}
+									{renderAppBar()}
+									<Toolbar />
+									<Component setTheme={setTheme} {...pageProps} />
+								</React.Fragment>
+							</main>
+						</div>
 					)}
 				</AppThemeConsumer>
 			</AppThemeProvider>
@@ -33,10 +110,36 @@ function MyApp(props) {
 	)
 }
 
-MyApp.getInitialProps = async ({ Component, ctx }) => {
+const useStyles = makeStyles( theme => ( {
+	root: {
+		width: '100%',
+		height: '100vh',
+		overflow: 'hidden',
+	},
+	content: {
+		display: 'flex',
+		flexDirection: 'column',
+		width: '100%',
+		height: '100%',
+		transition: theme.transitions.create( 'margin', {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen,
+		} ),
+		marginLeft: 0,
+	},
+	contentShift: ( { hasDrawer } ) => ( {
+		marginLeft: hasDrawer ? drawerWidth : 0,
+		transition: theme.transitions.create( 'margin', {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.enteringScreen,
+		} ),
+	} ),
+} ) )
+
+MyApp.getInitialProps = async ( { Component, ctx } ) => {
 	return {
 		pageProps: {
-			...(Component.getInitialProps ? await Component.getInitialProps(ctx) : {}),
+			...( Component.getInitialProps ? await Component.getInitialProps( ctx ) : {} ),
 			pathname: ctx.pathname,
 		},
 	}
@@ -47,4 +150,4 @@ MyApp.propTypes = {
 	pageProps: PropTypes.object.isRequired,
 }
 
-export default storeWrapper.withRedux(MyApp)
+export default storeWrapper.withRedux( MyApp )
