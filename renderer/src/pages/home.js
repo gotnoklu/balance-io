4dd39d0b5f4ad49d8d5c97d5b6f3df14b76a8101
Eli <cyronix787@gmail.com>
2021-02-20 09:22:01 +0000

    cleanup code

--- renderer/src/pages/home.js
+++ renderer/src/pages/home.js
@@ -1,28 +1,8 @@
 import React from 'react'
 import Box from '@material-ui/core/Box'
 import TaskBoardView from '../components/ui/TaskBoardView'
-import { useDispatch } from 'react-redux'
-import { getMainAppStore } from '../storage/electron-renderer'
-import { setPanelStore, setBoardStore, setTaskStore } from '../storage/redux/actions'
-import { getValueOfKey } from '../utilities/global'
-import { storageKeys } from '../constants'
 
 function HomePage() {
-	const dispatch = useDispatch()
-
-	React.useEffect( () => {
-		const getInitialDataFromStorage = async () => {
-			const storageResponse = await getMainAppStore()
-			if ( storageResponse.success ) {
-				dispatch( setBoardStore( getValueOfKey( storageResponse.data, storageKeys.BOARDS ) ) )
-				dispatch( setPanelStore( getValueOfKey( storageResponse.data, storageKeys.PANELS ) ) )
-				dispatch( setTaskStore( getValueOfKey( storageResponse.data, storageKeys.TASKS ) ) )
-			}
-		}
-
-		getInitialDataFromStorage()
-	}, [] )
-
 	return (
 		<Box width='100%' height='100%' component='section'>
 			<TaskBoardView />
