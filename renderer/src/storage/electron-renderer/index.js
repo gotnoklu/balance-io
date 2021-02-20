f18525383192237bd21c3bd7e6c39a8183457c4d
Eli <cyronix787@gmail.com>
2021-02-20 09:18:20 +0000

    cleanup code

--- renderer/src/storage/electron-renderer/index.js
+++ renderer/src/storage/electron-renderer/index.js
@@ -1,5 +1,5 @@
 import { ipcRenderer } from 'electron'
-import { createErrorResponse, createResponse } from '../../utilities/global'
+import { createErrorResponse } from '../../utilities/global'
 import {
 	SAVE_TO_MAIN_APP_STORE,
 	GET_MAIN_APP_STORE,
@@ -11,12 +11,12 @@ import {
 } from '../../constants/electron-renderer'
 
 export const getMainAppStore = async () => {
-	const nullStoreErrorResponse = await createErrorResponse( 'Main app store is empty.' )
-	const errorResponse = await createErrorResponse( 'Could not get main app store.' )
+	const nullStoreErrorResponse = await createErrorResponse( { message: 'Main app store is empty.' } )
+	const errorResponse = await createErrorResponse( { message: 'Could not get main app store.' } )
 	try {
-		const response = await ipcRenderer.invoke( GET_MAIN_APP_STORE )
-		if ( response.success ) {
-			return await createResponse( response.data, true )
+		const data = await ipcRenderer.invoke( GET_MAIN_APP_STORE )
+		if ( data.success ) {
+			return data
 		}
 		return nullStoreErrorResponse
 	} catch ( error ) {
@@ -25,79 +25,31 @@ export const getMainAppStore = async () => {
 }
 
 export const saveToMainAppStore = async ( key, value ) => {
-	const errorResponse = await createErrorResponse( 'Could not save to main app store.' )
-	try {
-		const response = await ipcRenderer.invoke( SAVE_TO_MAIN_APP_STORE, { key, value } )
-		if ( response.success ) {
-			return await createResponse( response.data, true )
-		}
-		return errorResponse
-	} catch ( error ) {
-		return errorResponse
-	}
+	const data = await ipcRenderer.invoke( SAVE_TO_MAIN_APP_STORE, { key, value } )
+	return data
 }
 
 export const deleteFromMainAppStore = async key => {
-	const errorResponse = await createErrorResponse( 'Could not delete from main app store.' )
-	try {
-		const response = await ipcRenderer.invoke( DELETE_FROM_MAIN_APP_STORE, key )
-		if ( response.success ) {
-			return await createResponse( response.data, true )
-		}
-		return errorResponse
-	} catch ( error ) {
-		return errorResponse
-	}
+	const data = await ipcRenderer.invoke( DELETE_FROM_MAIN_APP_STORE, key )
+	return data
 }
 
 export const clearMainAppStore = async () => {
-	const errorResponse = await createErrorResponse( 'Could not clear main app store.' )
-	try {
-		const response = await ipcRenderer.invoke( CLEAR_MAIN_APP_STORE )
-		if ( response.success ) {
-			return await createResponse( response.data, true )
-		}
-		return errorResponse
-	} catch ( error ) {
-		return errorResponse
-	}
+	const data = await ipcRenderer.invoke( CLEAR_MAIN_APP_STORE )
+	return data
 }
 
 export const getFromMainAppStore = async key => {
-	const errorResponse = await createErrorResponse( 'Could not get from main app store.' )
-	try {
-		const response = await ipcRenderer.invoke( GET_FROM_MAIN_APP_STORE, key )
-		if ( response.success ) {
-			return await createResponse( response.data, true )
-		}
-		return errorResponse
-	} catch ( error ) {
-		return errorResponse
-	}
+	const data = await ipcRenderer.invoke( GET_FROM_MAIN_APP_STORE, key )
+	return data
 }
 
 export const backupMainAppStore = async () => {
-	const errorResponse = await createErrorResponse( 'Could not backup main app store.' )
-	try {
-		const response = await ipcRenderer.invoke( SET_BACKUP_MAIN_APP_STORE )
-		if ( response.success ) {
-			return await createResponse( response.data, true )
-		}
-		return errorResponse
-	} catch ( error ) {
-		return errorResponse
-	}
+	const data = await ipcRenderer.invoke( SET_BACKUP_MAIN_APP_STORE )
+	return data
 }
 
 export const getBackupMainAppStore = async () => {
-	const errorResponse = await createErrorResponse( 'Could not get backup of main app store.' )
-	try {
-		const response = await ipcRenderer.invoke( GET_BACKUP_MAIN_APP_STORE )
-		if ( response.success ) {
-			return await createResponse( response.data, true )
-		}
-		return errorResponse
-	} catch ( error ) {
-		return errorResponse
-	}
+	const data = await ipcRenderer.invoke( GET_BACKUP_MAIN_APP_STORE )
+	return data
 }
